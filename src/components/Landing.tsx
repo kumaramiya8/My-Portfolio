df6f6f010eca4e97794350b7b454a8f93d3d97c4
kumaramiya8 <130311251+kumaramiya8@users.noreply.github.com>
2026-03-22 21:01:57 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -15,9 +15,9 @@ const Landing = ({ children }: PropsWithChildren) => {
             </h1>
           </div>
           <div className="landing-info">
-            <h3>A Support</h3>
+            <h3>A Senior</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Lead</div>
+              <div className="landing-h2-1">Prpduct Support</div>
               <div className="landing-h2-2">Engineer</div>
             </h2>
             <h2>
